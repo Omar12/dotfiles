88ff5e608918f352133280c68c093dc76212af05
Omar Martinez <this.is.omartinez@gmail.com>
2022-01-12 00:36:57 -0800

    updating gitconfig minor packages to install

--- npm.sh
+++ npm.sh
@@ -1,7 +1,9 @@
 npm #!/usr/bin/env bash
 
+npm i -g npx
 npm i -g typescript
 npm i -g nodemon
+npm i -g http-server
 npm i -g create-react-app
 npm i -g tldr
 npm i -g gatsby-cli
@@ -10,3 +12,6 @@ npm i -g now
 npm i -g vercel
 npm i -g json-server
 npm i -g netlify-cli
+npm i -g spotifydl
+npm i -g ava
+npm i -g tsc
